5109f043bf5c87ef100963716a4d50caac7ed929
River <ductran15601@gmail.com>
2021-03-05 17:32:51 +0700

    Update blog data

--- src/controllers/PagesController.js
+++ src/controllers/PagesController.js
@@ -1,20 +1,21 @@
 const {oauth2Client} = require('../middlewares/oauthClient');
+const {google} = require('googleapis');
+const blogger = google.blogger('v3');
 
 class PagesController {
-    index(req,res,next) {
+    async index(req,res,next) {
+        const blogData = await blogger.blogs.listByUser({userId: 'self'});
+        console.log(blogData)
         res.render('index',{
-            msg: "Hello bitches"
+            msg: "Hello bitches",
+            blogs: blogData.data.items
         });
     }
     async authCallback(req, res) {
         // const qs = new url.URL(req.url).searchParams;
         const code = req.query.code;
         const {tokens} = await oauth2Client.getToken(code);
-        oauth2Client.setCredentials({
-            access_token: tokens.access_token,
-            refresh_token: tokens.refresh_token,
-            expiry_date: true
-        });
+        oauth2Client.credentials = tokens;
         res.cookie('user', tokens.access_token, {
             expires: new Date(Date.now() + (3600 * 1000)),
             httpOnly: true
--- src/middlewares/oauthClient.js
+++ src/middlewares/oauthClient.js
@@ -8,7 +8,7 @@ if (fs.existsSync(keyPath)) {
     keys = require(keyPath).web;
 }
 // Create auth client
-const oauth2Client = new google.auth.OAuth2(keys.client_id, keys.client_secret, 'https://ductranspot.herokuapp.com/authCallback');
+const oauth2Client = new google.auth.OAuth2(keys.client_id, keys.client_secret, keys.redirect_uris[0]);
 oauth2Client.on('tokens', (tokens) => {
     if(tokens.refresh_token) {
         console.log(tokens.refresh_token);
@@ -23,16 +23,17 @@ async function processAuth(req,res,next) {
     if(!user) {
         const authUrl = oauth2Client.generateAuthUrl({
             access_type: 'offline',
-            scope: ['https://www.googleapis.com/auth/blogger']
+            scope: ['https://www.googleapis.com/auth/blogger','https://www.googleapis.com/auth/userinfo.profile']
         });
         res.render('authRequire', { authUrl });
     }else{
-        const tokenInfo = await oauth2Client.getTokenInfo(user);
-        console.log(tokenInfo);
+        const reAuth = await oauth2Client.setCredentials({
+            access_token: user
+        });
         next();
     }
 }
 
-module.exports = { processAuth, oauth2Client};
+module.exports = {processAuth, oauth2Client};
 
 
