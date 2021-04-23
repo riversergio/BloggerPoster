3b49ba2c0914a0342199ff17785faabccc243991
River Sergio <ductran15601@gmail.com>
2021-04-23 09:00:28 +0700

    Update

--- src/controllers/PagesController.js
+++ src/controllers/PagesController.js
@@ -1,10 +1,10 @@
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const blogger = google.blogger('v3');
 
 class PagesController {
-    async index(req,res) {
+    async index(req, res) {
         const blogData = await blogger.blogs.listByUser({ userId: 'self' });
-        const blogParseData = blogData.data.items.map((blog,index) => ({
+        const blogParseData = blogData.data.items.map((blog, index) => ({
             index: index + 1,
             id: blog.id,
             name: blog.name,
@@ -14,19 +14,19 @@ class PagesController {
             numPage: blog.pages.totalItems,
             published: [blog.published.substring(8, 10), blog.published.substring(5, 7), blog.published.substring(0, 4)].join("/")
         }));
-        
-        res.render('blogList',{
+
+        res.render('blogList', {
             title: "Blogger Poster - DucTranSpot",
             blogs: blogParseData,
             userData: res.locals.userData.data
         });
     }
-    signout(req,res) {
+    signout(req, res) {
         res.clearCookie('user');
         res.redirect('/');
     }
     async authCallback(req, res) {
-        res.redirect('back');
+        res.redirect('/');
     }
 }
 module.exports = new PagesController();
